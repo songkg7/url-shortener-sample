6e966df03d03ed8e86d13f14f177cac9e2244fa4
kgsong <kyunggeun.song@seavantage.com>
2023-05-04 11:59:58 +0900

    refactor: clean code

--- src/test/kotlin/com/example/urlshortener/urlpair/service/UrlShortenServiceTest.kt
+++ src/test/kotlin/com/example/urlshortener/urlpair/service/UrlShortenServiceTest.kt
@@ -11,31 +11,26 @@ import io.mockk.mockk
 import io.mockk.verify
 import java.util.*
 
-class UrlShortenServiceTest : FreeSpec() {
+class UrlShortenServiceTest : FreeSpec({
+    val urlPairRepository = mockk<UrlPairRepository>()
+    val urlShortenService = UrlShortenService(urlPairRepository)
 
-    override fun extensions() = listOf(SpringExtension)
-    private val urlPairRepository = mockk<UrlPairRepository>()
-
-    init {
-        val urlShortenService = UrlShortenService(urlPairRepository)
-        "longUrl 이 이미 DB 에 존재하면" - {
-            val urlPair = UrlPair(1, "2TX", "https://www.naver.com")
-            every { urlPairRepository.findByLongUrl(any()) } returns Optional.of(urlPair)
-            "shortUrl 을 반환한다." {
-                val shortUrl = urlShortenService.shorten("https://www.naver.com")
-                shortUrl shouldBe urlPair.shortUrl
-            }
-        }
-
-        "longUrl 이 DB 에 존재하지 않는다면" - {
-            every { urlPairRepository.findByLongUrl(any()) } returns Optional.empty()
-            "새로운 UrlPair 를 생성하고 shortUrl 을 반환한다." {
-                every { urlPairRepository.save(any<UrlPair>()) } returns UrlPair(1, "2TX", "https://www.naver.com")
-                val shortUrl = urlShortenService.shorten("https://www.naver.com")
-                shortUrl shouldBe "2TX"
-                verify(exactly = 1) { urlPairRepository.save(any<UrlPair>()) }
-            }
+    "longUrl 이 이미 DB 에 존재하면" - {
+        val urlPair = UrlPair(1, "2TX", "https://www.naver.com")
+        every { urlPairRepository.findByLongUrl(any()) } returns Optional.of(urlPair)
+        "shortUrl 을 반환한다." {
+            val shortUrl = urlShortenService.shorten("https://www.naver.com")
+            shortUrl shouldBe urlPair.shortUrl
         }
     }
 
-}
+    "longUrl 이 DB 에 존재하지 않는다면" - {
+        every { urlPairRepository.findByLongUrl(any()) } returns Optional.empty()
+        "새로운 UrlPair 를 생성하고 shortUrl 을 반환한다." {
+            every { urlPairRepository.save(any<UrlPair>()) } returns UrlPair(1, "2TX", "https://www.naver.com")
+            val shortUrl = urlShortenService.shorten("https://www.naver.com")
+            shortUrl shouldBe "2TX"
+            verify(exactly = 1) { urlPairRepository.save(any<UrlPair>()) }
+        }
+    }
+})
